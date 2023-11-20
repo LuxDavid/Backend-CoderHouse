5ffe71618839d8429010ffdbd9e721d5f4ae779b
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2023-11-19 19:15:18 -0600

    Practica_integradora totalmente finalizada

--- Practica_integradora/src/routes/views-router.js
+++ Practica_integradora/src/routes/views-router.js
@@ -1,28 +1,22 @@
 import { Router } from "express";
 import ProductManager from "../DAO/fileSystem/productManager.js";
-import ProductManagerMDB from "../DAO/mongoDB/productManagerMDB.js";
+import { productModel } from "../DAO/models/productModel.js";
 
 const router=Router();
 
 const productList= new ProductManager("./products.json");
 const products= await productList.getProducts();
 
-
-
-
 //---------------------------------------------------------------------
 
 router.get('/', async (req,res)=>{
 
-   
-
 try {
 
-    const productsDB= await new ProductManagerMDB().getProducts()
-    console.log(productsDB);
+    const productsDB= await productModel.find().lean().exec();
 
     res.render('home',{
-        products:products,
+        products:productsDB,
         style: 'index.css'
     });
 } catch (error) {
